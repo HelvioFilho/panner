4e74b48b7f901f29d56de94685274908b2925942
Hélvio Filho <helviosvf@gmail.com>
2024-07-12 12:13:44 -0300

    fix: fixes missing comma and indentation

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,11 @@
-import { Platform, TextInput, TextInputProps, View, ViewProps } from "react-native";
-import clsx from "clsx"
+import {
+  Platform,
+  TextInput,
+  TextInputProps,
+  View,
+  ViewProps,
+} from "react-native";
+import clsx from "clsx";
 
 import { colors } from "@/styles/colors";
 
@@ -8,26 +14,33 @@ type Variants = "primary" | "secondary" | "tertiary";
 type InputProps = ViewProps & {
   children: React.ReactNode;
   variant?: Variants;
-}
+};
 
-function Input({children, variant = "primary", className, ...rest}: InputProps){
+function Input({
+  children,
+  variant = "primary",
+  className,
+  ...rest
+}: InputProps) {
   return (
-    <View className={clsx(
-      "min-h-16 max-h-16 flex-row items-center gap-2",
-      {
-        "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
-        "bg-zinc-950": variant === "secondary",
-        "bg-zinc-900": variant === "tertiary",
-      },
-      className
-    )}
-    {...rest}>
-      { children }
+    <View
+      className={clsx(
+        "min-h-16 max-h-16 flex-row items-center gap-2",
+        {
+          "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
+          "bg-zinc-950": variant === "secondary",
+          "bg-zinc-900": variant === "tertiary",
+        },
+        className
+      )}
+      {...rest}
+    >
+      {children}
     </View>
-  )
+  );
 }
 
-function Field({...rest}: TextInputProps){
+function Field({ ...rest }: TextInputProps) {
   return (
     <TextInput
       className="flex-1 text-zinc-100 text-lg font-regular"
@@ -36,9 +49,9 @@ function Field({...rest}: TextInputProps){
       selectionColor={Platform.OS === "ios" ? colors.zinc[100] : undefined}
       {...rest}
     />
-  )
+  );
 }
 
 Input.Field = Field;
 
-export { Input }
+export { Input };
