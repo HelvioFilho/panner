839b8e556d9d48247776d711faf9d04ff4cf51fb
Hélvio Filho <helviosvf@gmail.com>
2024-07-21 20:26:01 -0300

    feat: Add Trip screen with activity and detail views
    - Implement Trip screen to display trip details and activities
    - Add logic to fetch trip details from the server using trip ID
    - Include loading state while fetching trip details
    - Allow toggling between activity and detail views
    - Use local search params to get trip ID and participant info
    - Apply custom styles using Tailwind classes

---
+++ src/app/trip/[id].tsx
@@ -0,0 +1,123 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { View, TouchableOpacity } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { CalendarRange, Info, MapPin, Settings2 } from "lucide-react-native";
+
+import { Details } from "./details";
+import { Activities } from "./activities";
+
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { Loading } from "@/components/Loading";
+
+import { colors } from "@/styles/colors";
+import { TripDetails, tripServer } from "@/server/trip-server";
+
+export type TripData = TripDetails & {
+  when: string;
+};
+
+export default function Trip() {
+  const [isLoadingTrip, setIsLoadingTrip] = useState(true);
+
+  const [tripDetails, setTripDetails] = useState({} as TripData);
+  const [option, setOption] = useState<"activity" | "details">("activity");
+
+  const tripParams = useLocalSearchParams<{
+    id: string;
+    participant?: string;
+  }>();
+
+  async function getTripDetails() {
+    try {
+      setIsLoadingTrip(true);
+
+      if (!tripParams.id) {
+        return router.back();
+      }
+
+      const trip = await tripServer.getById(tripParams.id);
+
+      const maxLengthDestination = 14;
+      const destination =
+        trip.destination.length > maxLengthDestination
+          ? trip.destination.slice(0, maxLengthDestination) + "..."
+          : trip.destination;
+
+      const starts_at = dayjs(trip.starts_at).format("DD");
+      const ends_at = dayjs(trip.ends_at).format("DD");
+      const month = dayjs(trip.starts_at).format("MMM");
+
+      setTripDetails({
+        ...trip,
+        when: `${destination} de ${starts_at} a ${ends_at} de ${month}.`,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoadingTrip(false);
+    }
+  }
+
+  useEffect(() => {
+    getTripDetails();
+  }, []);
+
+  if (isLoadingTrip) {
+    return <Loading />;
+  }
+
+  return (
+    <View className="flex-1 px-5 pt-16">
+      <Input variant="tertiary">
+        <MapPin color={colors.zinc[400]} size={20} />
+        <Input.Field value={tripDetails.when} readOnly />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          className="w-9 h-9 bg-zinc-800 items-center justify-center rounded"
+          onPress={() => {}}
+        >
+          <Settings2 color={colors.zinc[400]} size={20} />
+        </TouchableOpacity>
+      </Input>
+
+      {option === "activity" ? (
+        <Activities tripDetails={tripDetails} />
+      ) : (
+        <Details tripId={tripDetails.id} />
+      )}
+
+      <View className="w-full absolute -bottom-1 self-center justify-end pb-5 z-10 bg-zinc-950">
+        <View className="w-full flex-row bg-zinc-900 p-4 rounded-lg border border-zinc-800 gap-2">
+          <Button
+            className="flex-1"
+            onPress={() => setOption("activity")}
+            variant={option === "activity" ? "primary" : "secondary"}
+          >
+            <CalendarRange
+              color={
+                option === "activity" ? colors.lime[950] : colors.zinc[200]
+              }
+              size={20}
+            />
+            <Button.Title>Atividades</Button.Title>
+          </Button>
+
+          <Button
+            className="flex-1"
+            onPress={() => setOption("details")}
+            variant={option === "details" ? "primary" : "secondary"}
+          >
+            <Info
+              color={option === "details" ? colors.lime[950] : colors.zinc[200]}
+              size={20}
+            />
+            <Button.Title>Detalhes</Button.Title>
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+}
