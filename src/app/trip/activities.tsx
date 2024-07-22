a7019095fdc7abf4f1beff9404b080ecfb0488a2
Hélvio Filho <helviosvf@gmail.com>
2024-07-22 13:11:59 -0300

    feat: Add create trip activity functionality in Activities component
    - Implement modal for creating new activities
    - Add state management for creating activities, including title, date, and time
    - Integrate dayjs for date and time manipulation
    - Use SectionList for displaying trip activities
    - Handle form validation and display alerts for invalid inputs
    - Connect to activities server to create new activities

--- src/app/trip/activities.tsx
+++ src/app/trip/activities.tsx
@@ -1,11 +1,21 @@
-import { SectionList, Text, View } from "react-native";
+import dayjs from "dayjs";
+import { Alert, Keyboard, SectionList, Text, View } from "react-native";
 import { TripData } from "./[id]";
 import { Button } from "@/components/Button";
-import { PlusIcon } from "lucide-react-native";
+import {
+  PlusIcon,
+  Tag,
+  Calendar as IconCalendar,
+  Clock,
+} from "lucide-react-native";
 import { colors } from "@/styles/colors";
 import { Activity, ActivityProps } from "@/components/Activity";
 import { useState } from "react";
 import { Loading } from "@/components/Loading";
+import { Modal } from "@/components/Modal";
+import { Input } from "@/components/Input";
+import { Calendar } from "@/components/Calendar";
+import { activitiesServer } from "@/server/activities-server";
 
 type ActivitiesProps = {
   tripDetails: TripData;
@@ -27,11 +37,79 @@ type TripActivities = {
 
 export function Activities({ tripDetails }: ActivitiesProps) {
   const [showModal, setShowModal] = useState(MODAL.NONE);
+
+  const [isCreatingActivity, setIsCreatingActivity] = useState(false);
   const [isLoadingActivities, setIsLoadingActivities] = useState(false);
+
+  const [activityTitle, setActivityTitle] = useState("");
+  const [activityDate, setActivityDate] = useState("");
+  const [activityHour, setActivityHour] = useState("");
 
   const [tripActivities, setTripActivities] = useState<TripActivities[]>([]);
 
+  function resetNewActivityFields() {
+    setActivityDate("");
+    setActivityTitle("");
+    setActivityHour("");
+    setShowModal(MODAL.NONE);
+  }
+
+  async function handleCreateTripActivity() {
+    try {
+      if (!activityTitle || !activityDate || !activityHour) {
+        return Alert.alert("Aviso", "Preencha todos os campos!");
+      }
+
+      setIsCreatingActivity(true);
+
+      const [hours, minutes] = activityHour.split(":").map(Number);
+
+      const occursAt = dayjs(activityDate)
+        .set("hour", hours)
+        .set("minute", minutes)
+        .tz("America/Sao_Paulo", true)
+        .toString();
+
+      await activitiesServer.create({
+        tripId: tripDetails.id,
+        occurs_at: occursAt,
+        title: activityTitle,
+      });
+
+      Alert.alert("Aviso", "Nova atividade cadastrada com sucesso!");
+
+      await getTripActivities();
+      resetNewActivityFields();
+    } catch (error) {
+      Alert.alert(
+        "Aviso",
+        "Algo deu errado e não foi possível criar uma nova atividade!"
+      );
+    } finally {
+      setIsCreatingActivity(false);
+    }
+  }
+
   async function getTripActivities() {}
+
+  function handleTimeChange(text: string) {
+    let formattedText = text.replace(/[^0-9]/g, "");
+
+    if (formattedText.length > 2) {
+      formattedText = `${formattedText.slice(0, 2)}:${formattedText.slice(
+        2,
+        4
+      )}`;
+    }
+
+    if (formattedText.length === 5) {
+      const [hours, minutes] = formattedText.split(":").map(Number);
+      if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+        return Alert.alert("Aviso", "Horário inválido!");
+      }
+    }
+    setActivityHour(formattedText);
+  }
 
   return (
     <View className="flex-1">
@@ -39,7 +117,7 @@ export function Activities({ tripDetails }: ActivitiesProps) {
         <Text className="flex-1 text-zinc-50 text-2xl font-semibold">
           Atividades
         </Text>
-        <Button onPress={() => {}}>
+        <Button onPress={() => setShowModal(MODAL.NEW_ACTIVITY)}>
           <Button.Title>Nova Atividade</Button.Title>
           <PlusIcon color={colors.lime[950]} />
         </Button>
@@ -72,6 +150,78 @@ export function Activities({ tripDetails }: ActivitiesProps) {
           showsVerticalScrollIndicator={false}
         />
       )}
+      <Modal
+        visible={showModal === MODAL.NEW_ACTIVITY}
+        title="Cadastrar atividade"
+        subtitle="Todos os convidados podem visualizar as atividades"
+        onClose={() => setShowModal(MODAL.NONE)}
+      >
+        <View className="mt-4 mb-3">
+          <Input variant="secondary">
+            <Tag color={colors.zinc[400]} size={20} />
+            <Input.Field
+              placeholder="Qual atividade?"
+              onChangeText={setActivityTitle}
+              value={activityTitle}
+            />
+          </Input>
+
+          <View className="w-full mt-2 flex-row gap-2">
+            <Input variant="secondary" className="flex-1">
+              <IconCalendar color={colors.zinc[400]} size={20} />
+              <Input.Field
+                placeholder="Data"
+                onChangeText={setActivityTitle}
+                value={
+                  activityDate ? dayjs(activityDate).format("DD [de] MMMM") : ""
+                }
+                onFocus={() => Keyboard.dismiss()}
+                showSoftInputOnFocus={false}
+                onPressIn={() => setShowModal(MODAL.CALENDAR)}
+              />
+            </Input>
+
+            <Input variant="secondary" className="flex-1">
+              <Clock color={colors.zinc[400]} size={20} />
+              <Input.Field
+                placeholder="Horário?"
+                onChangeText={handleTimeChange}
+                value={activityHour}
+                keyboardType="numeric"
+                maxLength={5}
+              />
+            </Input>
+          </View>
+        </View>
+
+        <Button
+          onPress={handleCreateTripActivity}
+          isLoading={isCreatingActivity}
+        >
+          <Button.Title>Salvar atividade</Button.Title>
+        </Button>
+      </Modal>
+
+      <Modal
+        title="Selecionar data"
+        subtitle="Selecione a data da atividade"
+        visible={showModal === MODAL.CALENDAR}
+        onClose={() => setShowModal(MODAL.NONE)}
+      >
+        <View className="gap-4 mt-4">
+          <Calendar
+            onDayPress={(day) => setActivityDate(day.dateString)}
+            markedDates={{ [activityDate]: { selected: true } }}
+            initialDate={tripDetails.starts_at.toString()}
+            minDate={tripDetails.starts_at.toString()}
+            maxDate={tripDetails.ends_at.toString()}
+          />
+
+          <Button onPress={() => setShowModal(MODAL.NEW_ACTIVITY)}>
+            <Button.Title>Confirmar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
