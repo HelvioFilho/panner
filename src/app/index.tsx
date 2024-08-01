1c78cb2eb41311cc637d52f3b803321ba49196c2
Hélvio Filho <helviosvf@gmail.com>
2024-08-01 12:13:31 -0300

    feat: implement social login and terms of use modal
    - Implemented social login functionality for Google, Apple, and Facebook using @clerk/clerk-expo.
    - Added a modal for displaying Terms of Use and Privacy Policy.
    - Set up WebBrowser with expo-web-browser and expo-linking for OAuth flow.
    - Introduced state management for loading indicator and modal visibility.
    - Updated UI with SocialButton component for each login method.
    - Enhanced user feedback with an ActivityIndicator during login process.
    - Included comprehensive Terms of Use text in a scrollable modal view.
    - Adjusted app's main layout to support authentication flow and terms agreement.
    
    **Note:** The changes ensure a smooth user experience and compliance with user data handling policies.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,181 +1,70 @@
-import dayjs from "dayjs";
 import { useEffect, useState } from "react";
-import { router } from "expo-router";
-import { Input } from "@/components/Input";
-import { Alert, Image, Keyboard, Pressable, Text, View } from "react-native";
-import { DateData } from "react-native-calendars";
-
 import {
-  MapPin,
-  Calendar as IconCalendar,
-  Settings2,
-  UserRoundPlus,
-  ArrowRight,
-  AtSign,
-} from "lucide-react-native";
+  ActivityIndicator,
+  Image,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import * as Linking from "expo-linking";
+import * as WebBrowser from "expo-web-browser";
+import { StartOAuthFlowReturnType, useOAuth } from "@clerk/clerk-expo";
 
 import { Modal } from "@/components/Modal";
 import { Button } from "@/components/Button";
-import { Loading } from "@/components/Loading";
-import { Calendar } from "@/components/Calendar";
-import { GuestEmail } from "@/components/GuestEmail";
+import { SocialButton } from "@/components/SocialButton";
 
 import { colors } from "@/styles/colors";
-import { tripStorage } from "@/storage/trip";
-import { tripServer } from "@/server/trip-server";
-import { validateInput } from "@/utils/validateInput";
-import { calendarUtils, DatesSelected } from "@/utils/calendarUtils";
 
-enum StepForm {
-  TRIP_DETAILS = 1,
-  ADD_EMAIL = 2,
-}
+WebBrowser.maybeCompleteAuthSession();
 
-enum MODAL {
-  NONE = 0,
-  CALENDAR = 1,
-  GUESTS = 2,
-}
+export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-export default function Index() {
-  const [isCreatingTrip, setIsCreatingTrip] = useState(false);
-  const [isGettingTrip, setIsGettingTrip] = useState(true);
+  const oauthGoogle = useOAuth({ strategy: "oauth_google" });
+  const oauthApple = useOAuth({ strategy: "oauth_apple" });
+  const oauthFacebook = useOAuth({ strategy: "oauth_facebook" });
 
-  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS);
-  const [selectedDates, setSelectedDates] = useState({} as DatesSelected);
-  const [destination, setDestination] = useState("");
-  const [emailToInvite, setEmailToInvite] = useState("");
-  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
-
-  const [showModal, setShowModal] = useState(MODAL.NONE);
-
-  function handleNextStepForm() {
-    if (
-      destination.trim().length === 0 ||
-      !selectedDates.startsAt ||
-      !selectedDates.endsAt
-    ) {
-      return Alert.alert(
-        "Aviso",
-        "Preencha todos as informações da viagem para seguir"
-      );
-    }
-
-    if (destination.length < 4) {
-      return Alert.alert("Aviso", "O destino deve ter pelo menos 4 caracteres");
-    }
-
-    if (stepForm === StepForm.TRIP_DETAILS) {
-      return setStepForm(StepForm.ADD_EMAIL);
-    }
-
-    Alert.alert("Aviso", "Confirmar viagem?", [
-      {
-        text: "Não",
-        style: "cancel",
-      },
-      {
-        text: "Sim",
-        onPress: createTrip,
-      },
-    ]);
-  }
-
-  function handleSelectDate(selectedDay: DateData) {
-    const dates = calendarUtils.orderStartsAtAndEndsAt({
-      startsAt: selectedDates.startsAt,
-      endsAt: selectedDates.endsAt,
-      selectedDay,
-    });
-
-    setSelectedDates(dates);
-  }
-
-  function handleRemoveEmail(emailToRemove: string) {
-    setEmailsToInvite((prevState) =>
-      prevState.filter((email) => email !== emailToRemove)
-    );
-  }
-
-  function handleAddEmail() {
-    if (!validateInput.email(emailToInvite)) {
-      return Alert.alert("Convidado", "E-mail inválido!");
-    }
-
-    const emailAlreadyExists = emailsToInvite.find(
-      (email) => email === emailToInvite
-    );
-
-    if (emailAlreadyExists) {
-      return Alert.alert("Convidado", "E-mail já foi adicionado!");
-    }
-
-    setEmailsToInvite((prevState) => [...prevState, emailToInvite]);
-    setEmailToInvite("");
-  }
-
-  async function saveTrip(tripId: string) {
+  async function handleSocialLogin(type: string) {
     try {
-      await tripStorage.save(tripId);
-      router.navigate("/trip/" + tripId);
-    } catch (error) {
-      Alert.alert(
-        "Aviso",
-        "Não foi possível salvar o id da viagem no dispositivo."
-      );
-      console.log(error);
-    }
-  }
-
-  async function createTrip() {
-    try {
-      setIsCreatingTrip(true);
-
-      const newTrip = await tripServer.create({
-        destination,
-        starts_at: dayjs(selectedDates.startsAt?.dateString).toString(),
-        ends_at: dayjs(selectedDates.endsAt?.dateString).toString(),
-        emails_to_invite: emailsToInvite,
-      });
-
-      Alert.alert("Aviso", "Viagem criada com sucesso!", [
-        {
-          text: "OK. Continuar.",
-          onPress: () => saveTrip(newTrip.tripId),
-        },
-      ]);
-    } catch (error) {
-      console.log(error);
-      setIsCreatingTrip(false);
-    }
-  }
-
-  async function getTrip() {
-    try {
-      const tripID = await tripStorage.get();
-
-      if (!tripID) {
-        return setIsGettingTrip(false);
+      setIsLoading(true);
+      const redirectUrl = Linking.createURL("/");
+      let oAuthFlow = {} as StartOAuthFlowReturnType;
+      switch (type) {
+        case "google":
+          oAuthFlow = await oauthGoogle.startOAuthFlow({ redirectUrl });
+          break;
+        case "apple":
+          oAuthFlow = await oauthApple.startOAuthFlow({ redirectUrl });
+          break;
+        case "facebook":
+          oAuthFlow = await oauthFacebook.startOAuthFlow({ redirectUrl });
+          break;
       }
 
-      const trip = await tripServer.getById(tripID);
-
-      if (trip) {
-        return router.navigate("/trip/" + trip.id);
+      if (oAuthFlow.authSessionResult?.type === "success") {
+        if (oAuthFlow.setActive) {
+          await oAuthFlow.setActive({ session: oAuthFlow.createdSessionId });
+        }
+      } else {
+        setIsLoading(false);
       }
     } catch (error) {
-      setIsGettingTrip(false);
-      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
-    getTrip();
-  }, []);
+    WebBrowser.warmUpAsync();
 
-  if (isGettingTrip) {
-    return <Loading />;
-  }
+    return () => {
+      WebBrowser.coolDownAsync();
+    };
+  });
 
   return (
     <View className="flex-1 items-center justify-center px-5">
@@ -184,151 +73,87 @@ export default function Index() {
         className="h-8"
         resizeMode="contain"
       />
-
       <Image source={require("@/assets/bg.png")} className="absolute" />
-
-      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
-        Convide seus amigos e planeje sua{"\n"}próxima viagem
+      <Text className="text-zinc-100 font-regular text-lg mt-3">
+        O seu aplicativo de planejamento de viagem.
       </Text>
-
-      <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
-        <Input>
-          <MapPin color={colors.zinc[400]} size={20} />
-          <Input.Field
-            placeholder="Para onde?"
-            editable={stepForm === StepForm.TRIP_DETAILS}
-            onChangeText={setDestination}
-            value={destination}
-          />
-        </Input>
-        <Input>
-          <IconCalendar color={colors.zinc[400]} size={20} />
-          <Input.Field
-            placeholder="Quando?"
-            editable={stepForm === StepForm.TRIP_DETAILS}
-            onFocus={() => Keyboard.dismiss()}
-            showSoftInputOnFocus={false}
-            onPressIn={() =>
-              stepForm === StepForm.TRIP_DETAILS && setShowModal(MODAL.CALENDAR)
-            }
-            value={selectedDates.formatDatesInText}
-          />
-        </Input>
-        {stepForm === StepForm.ADD_EMAIL && (
-          <>
-            <View className="border-b py-3 border-zinc-800">
-              <Button
-                variant="secondary"
-                onPress={() => setStepForm(StepForm.TRIP_DETAILS)}
-              >
-                <Button.Title>Alterar local/data</Button.Title>
-                <Settings2 color={colors.zinc[200]} size={20} />
-              </Button>
-            </View>
-            <Pressable
-              className="flex-row items-center gap-2 pt-4 pb-5"
-              onPress={() => {
-                setShowModal(MODAL.GUESTS);
-              }}
-            >
-              <UserRoundPlus
-                color={
-                  emailsToInvite.length > 0
-                    ? colors.zinc[100]
-                    : colors.zinc[400]
-                }
-                size={20}
-              />
-              <Text
-                className={`${
-                  emailsToInvite.length > 0 ? "text-zinc-100" : "text-zinc-400"
-                } font-regular text-lg pt-1`}
-              >
-                {emailsToInvite.length > 0
-                  ? `${emailsToInvite.length} ${
-                      emailsToInvite.length > 1
-                        ? "pessoas convidadas"
-                        : "pessoa convidada"
-                    }`
-                  : "Quem estará na viagem?"}
-              </Text>
-            </Pressable>
-          </>
-        )}
-
-        <Button onPress={handleNextStepForm} isLoading={isCreatingTrip}>
-          <Button.Title>
-            {stepForm === StepForm.TRIP_DETAILS
-              ? "Continuar"
-              : "Confirmar Viagem"}
-          </Button.Title>
-          <ArrowRight color={colors.lime[950]} size={20} />
-        </Button>
-      </View>
-      <Text className="text-zinc-500 font-regular text-center text-base">
-        Ao planejar sua viagem pela plann.er você automaticamente concorda com
-        nossos{" "}
-        <Text className="text-zinc-300 underline">
-          termos de uso e políticas de privacidade.
-        </Text>
-      </Text>
-
-      <Modal
-        title="Selecionar datas"
-        subtitle="Selecione a data de ida e volta da viagem"
-        visible={showModal === MODAL.CALENDAR}
-        onClose={() => setShowModal(MODAL.NONE)}
-      >
-        <View className="gap-4 mt-4">
-          <Calendar
-            minDate={dayjs().toISOString()}
-            onDayPress={handleSelectDate}
-            markedDates={selectedDates.dates}
-          />
-
-          <Button onPress={() => setShowModal(MODAL.NONE)}>
-            <Button.Title>Confirmar</Button.Title>
-          </Button>
+      {isLoading ? (
+        <View className="items-center justify-center mt-6">
+          <ActivityIndicator color={colors.lime[300]} />
+          <Text className="text-zinc-100 font-regular text-lg mt-3">
+            Conectando...
+          </Text>
         </View>
-      </Modal>
-      <Modal
-        title="Selecionar convidados"
-        subtitle="Os convidados irão receber e-mails para confirmar a participação na viagem."
-        visible={showModal === MODAL.GUESTS}
-        onClose={() => setShowModal(MODAL.NONE)}
-      >
-        <View className="my-2 flex-wrap gap-2 border-b border-zinc-800 py-5 items-start">
-          {emailsToInvite.length > 0 ? (
-            emailsToInvite.map((email) => (
-              <GuestEmail
-                key={email}
-                email={email}
-                onRemove={() => handleRemoveEmail(email)}
-              />
-            ))
-          ) : (
-            <Text className="text-zinc-600 text-base font-regular">
-              Nenhum e-mail adicionado.
-            </Text>
-          )}
-        </View>
-
-        <View className="gap-4 mt-4">
-          <Input variant="secondary">
-            <AtSign color={colors.zinc[400]} size={20} />
-            <Input.Field
-              placeholder="Digite o e-mail do convidado"
-              keyboardType="email-address"
-              onChangeText={(text) => setEmailToInvite(text.toLowerCase())}
-              value={emailToInvite}
-              returnKeyType="send"
-              onSubmitEditing={handleAddEmail}
+      ) : (
+        <View className="gap-3 mt-6">
+          <SocialButton
+            platform="google"
+            onPress={() => handleSocialLogin("google")}
+          />
+          {Platform.OS === "ios" && (
+            <SocialButton
+              platform="apple"
+              onPress={() => handleSocialLogin("apple")}
             />
-          </Input>
-          <Button onPress={handleAddEmail}>
-            <Button.Title>Convidar</Button.Title>
-          </Button>
+          )}
+          <SocialButton
+            platform="facebook"
+            onPress={() => handleSocialLogin("facebook")}
+          />
         </View>
+      )}
+      <View className="mt-7">
+        <Text className="text-zinc-500 font-regular text-center text-base">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda com
+          nossos{" "}
+          <Pressable onPress={() => setShowModal(true)}>
+            <Text className="text-zinc-300 underline">
+              termos de uso e políticas de privacidade.
+            </Text>
+          </Pressable>
+        </Text>
+      </View>
+      <Modal
+        title="Termos de Uso"
+        subtitle="Por favor, leia atentamente os termos de uso."
+        visible={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <ScrollView className="mt-4 px-4" showsVerticalScrollIndicator={false}>
+          <Text className="text-zinc-50 text-lg font-semibold mb-2">
+            1. Sobre o uso de e-mail e nome de usuário
+          </Text>
+          <Text className="text-base text-zinc-400">
+            Informamos que o aplicativo utiliza o e-mail e nome do usuário
+            unicamente para visualização e controle de cadastro de usuários.
+            {"\n"}
+            {"\n"}
+            Nenhuma informação será utilizada para fins comerciais ou venda de
+            dados de qualquer espécie.
+          </Text>
+          <Text className="text-zinc-50 text-lg font-semibold mt-4 mb-2">
+            2. Sobre responsabilidade
+          </Text>
+          <Text className="text-base text-zinc-400">
+            O aplicativo não se responsabiliza por atrasos na viagem ou nas
+            atividades. {"\n"}
+            {"\n"} O usuário está ciente de que o aplicativo apenas mostra as
+            atividades e não é responsável por elas. {"\n"}
+            {"\n"}Os links compartilhados são de inteira responsabilidade dos
+            usuários que os compartilharam, deixando o aplicativo sem nenhuma
+            responsabilidade sobre eles.
+          </Text>
+          <Text className="text-zinc-50 text-lg font-semibold mt-4 mb-2">
+            3. Aceitação dos Termos
+          </Text>
+          <Text className="text-base text-zinc-400">
+            Ao utilizar o aplicativo, o usuário concorda com todos os termos
+            acima.
+          </Text>
+        </ScrollView>
+        <Button onPress={() => setShowModal(false)} className="mt-7">
+          <Button.Title>Fechar</Button.Title>
+        </Button>
       </Modal>
     </View>
   );
